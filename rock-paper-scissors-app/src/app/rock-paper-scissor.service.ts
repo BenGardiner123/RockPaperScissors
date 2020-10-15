5eee264994014181c92382cfb0f3e22f954baaf5
Ben Gardiner <bennygards@gmail.com>
2020-10-15 11:59:47 +1100

    added ability to access the private selection prop

--- rock-paper-scissors-app/src/app/rock-paper-scissor.service.ts
+++ rock-paper-scissors-app/src/app/rock-paper-scissor.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +10,21 @@ import { Injectable } from '@angular/core';
 
 export class RockPaperScissorService {
 
-  private selection: 'rock' | 'paper' | 'scissors' | null;
+  private _selection: 'rock' | 'paper' | 'scissors' | null;
 
-  constructor() { }
+  get selection(){
+    return this._selection;
+  }
+
+  constructor(private router: Router) { }
 
 
   commitSelection(option: 'rock' | 'paper' | 'scissors'){
-
-  }
-
+    //the null here will be replaced by the get() request to the endpoint
+    of(null).pipe(delay(3000)).subscribe(() => {
+    //this stores the selection being pushed over from the compnent into the variable above
+    this._selection = option;
+    this.router.navigateByUrl("/result");
+  })
+ }
 }
--- rock-paper-scissors-app/src/app/routes/rps-display-result/rps-display-result.component.ts
+++ rock-paper-scissors-app/src/app/routes/rps-display-result/rps-display-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RockPaperScissorService } from 'src/app/rock-paper-scissor.service';
 
 @Component({
   selector: 'app-rps-display-result',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RpsDisplayResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(public rockPaperScissorsService: RockPaperScissorService) { }
 
   ngOnInit(): void {
   }
