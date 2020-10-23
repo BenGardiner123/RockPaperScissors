e92074498fc4384504e3e40bf2e02d743aa5717b
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-10-23 16:11:25 +1100

    dissapearing from the dom error

--- rock-paper-scissors-app/src/app/routes/rps-selection/rps-selection.component.ts
+++ rock-paper-scissors-app/src/app/routes/rps-selection/rps-selection.component.ts
@@ -12,6 +12,7 @@ export class RpsSelectionComponent implements OnInit {
  public paperSelected = false;
  public scissorsSelected = false;
 
+
   constructor( private rockPaperScissorService: RockPaperScissorService) { 
     
   }
