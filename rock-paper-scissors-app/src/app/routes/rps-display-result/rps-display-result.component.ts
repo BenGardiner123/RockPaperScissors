5d7ba2e934248d08cec14a2a67850bb1b48cbf6e
Ben Gardiner <bennygards@gmail.com>
2020-10-27 00:29:20 +1100

    selected images from the service in to the div

--- rock-paper-scissors-app/src/app/routes/rps-display-result/rps-display-result.component.ts
+++ rock-paper-scissors-app/src/app/routes/rps-display-result/rps-display-result.component.ts
@@ -7,15 +7,58 @@ import { RockPaperScissorService } from 'src/app/rock-paper-scissor.service';
   templateUrl: './rps-display-result.component.html',
   styleUrls: ['./rps-display-result.component.css']
 })
+
+
 export class RpsDisplayResultComponent implements OnInit {
 
-  constructor(public rockPaperScissorsService: RockPaperScissorService, private router: Router) { }
+  public aiBackground: string = "";
+  public userBackground: string = "";
+
+  constructor(public rockPaperScissorsService: RockPaperScissorService, private router: Router) { 
+    this.rockPaperScissorsService = rockPaperScissorsService;
+    this.aiBackgroundSetter();
+    this.userBackgroundSetter();
+  }
 
   ngOnInit(): void {
+
+    
     
     if(this.rockPaperScissorsService.selection == null){
       this.router.navigateByUrl("/selection");
     } 
   }
+
+  aiBackgroundSetter(){
+    if (this.rockPaperScissorsService.aiselection == "Rock")
+    {
+      this.aiBackground = "https://i.ibb.co/VqKy2hT/rock.png";
+    }
+    if (this.rockPaperScissorsService.aiselection == "Paper")
+    {
+      this.aiBackground = "https://i.ibb.co/zXMN3xJ/paper.png";
+    }
+    else if (this.rockPaperScissorsService.aiselection == "Scissors")
+    {
+      this.aiBackground = "https://i.ibb.co/BPxfRJD/scissors1.png";
+    }
+  }
+
+    userBackgroundSetter(){
+      if (this.rockPaperScissorsService.selection == "Rock")
+    {
+      this.userBackground = "https://i.ibb.co/VqKy2hT/rock.png";
+    }
+    if (this.rockPaperScissorsService.selection == "Paper")
+    {
+      this.userBackground = "https://i.ibb.co/zXMN3xJ/paper.png";
+    }
+    else if (this.rockPaperScissorsService.selection == "Scissors")
+    {
+      this.userBackground = "https://i.ibb.co/BPxfRJD/scissors1.png";
+    }
+    
+  }
+
 
 }
